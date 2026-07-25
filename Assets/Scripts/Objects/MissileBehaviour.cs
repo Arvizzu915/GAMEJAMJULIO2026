9e5aac225e39bc5304c004543dacc718b5dad0d3
DavidElPapu <david.jung@uniat.edu.mx>
2026-07-25 00:00:59 -0600

    IDestructible interface added, and sprites for lifesaver and missile, algon with missile animation

--- Assets/Scripts/Objects/MissileBehaviour.cs
+++ Assets/Scripts/Objects/MissileBehaviour.cs
@@ -4,14 +4,25 @@ public class MissileBehaviour : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D rb;
+    private Animator animator;
 
     private void Awake()
     {
         TryGetComponent(out rb);
+        TryGetComponent(out animator);
     }
 
     public void OnSpawn(Vector3 shootDir)
     {
         rb.AddForce(shootDir * speed, ForceMode2D.Impulse);
+        animator.Play("MissileIdle");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out IDestructible destructible))
+        {
+            destructible.DestroyObject();
+        }
     }
 }
